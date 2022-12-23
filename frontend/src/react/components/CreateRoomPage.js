7d68baeefbae798b652deaec01db7d06fd6a5968
Morgan Bradford <morganjbradford95@gmail.com>
2022-12-23 14:41:38 -0600

    create form for creating a room

--- frontend/src/react/components/CreateRoomPage.js
+++ frontend/src/react/components/CreateRoomPage.js
@@ -1,7 +1,86 @@
 import React from 'react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  Grid,
+  Paper,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+
 
 export default function CreateRoomPage() {
+  const defaultVotes = 2;
+
   return (
-   <h1>Create Room Page</h1>
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography
+          component='h4'
+          variant='h4'
+        >
+          Create A Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <FormControl
+          component='fieldset'
+        >
+          <FormHelperText>
+            <div align='center'>
+              Guest Control of Playback State
+            </div>
+          </FormHelperText>
+          <RadioGroup row defaultValue='true'>
+            <FormControlLabel
+              value='true'
+              control={
+                <Radio color="primary"/>
+              }
+              label='Play/Pause'
+              labelPlacement='bottom'
+            />
+            <FormControlLabel
+              value='false'
+              control={
+                <Radio color="secondary"/>
+              }
+              label='No Control'
+              labelPlacement='bottom'
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align='center'>
+        <FormControl>
+          <TextField
+            required={true}
+            type='number'
+            defaultValue={defaultVotes}
+            inputProps={{
+              min: 1,
+              style: {textAlign: 'center'}
+            }}
+          />
+          <FormHelperText>
+            <div align='center'>
+              Votes Required to Skip Song
+            </div>
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align='center'>
+        <Button color='primary' variant='contained'>Create a Room</Button>
+      </Grid>
+      <Grid item xs={12} align='center'>
+        <Button color='secondary' variant='contained' to='/' component={Link}>Back</Button>
+      </Grid>
+    </Grid>
   );
 }
