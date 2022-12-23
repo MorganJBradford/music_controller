fdb72393c1363e43c90938a399f8a66194becfda
Morgan Bradford <morganjbradford95@gmail.com>
2022-12-23 13:24:38 -0600

    add react router

--- frontend/src/react/components/HomePage.js
+++ frontend/src/react/components/HomePage.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import CreateRoomPage from './CreateRoomPage';
+import JoinRoomPage from './JoinRoomPage';
 
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<p>This is the home page</p>}/>
+        <Route path='/join' element={<JoinRoomPage/>}/>
+        <Route path='/create' element={<CreateRoomPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
