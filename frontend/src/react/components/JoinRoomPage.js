dd157582c75cbb5d5f603a593a9039cbf9325619
Morgan Bradford <morganjbradford95@gmail.com>
2022-12-24 18:41:59 -0600

    connect api to frontend

--- frontend/src/react/components/JoinRoomPage.js
+++ frontend/src/react/components/JoinRoomPage.js
@@ -1,16 +1,33 @@
 import React, { useState } from 'react';
 import { TextField, Button, Grid, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
 export default function JoinRoomPage() {
   const [roomCode, setRoomCode] = useState(null);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleTextChange = (e) => {
     setRoomCode(e.target.value);
   }
 
   const roomButtonPressed = () => {
-
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        code: roomCode
+      })
+    };
+    fetch('/api/join-room', requestOptions).then((response) => {
+      if (response.ok) {
+        navigate(`/room/${roomCode}`)
+      } else {
+        setError({error: 'Room not found.'});
+      }
+    }).catch((error) => {
+      console.log(error);
+    })
   }
 
   return (
