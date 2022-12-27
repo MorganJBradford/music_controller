f1afa515c4dee8b1f983414a620dc32d2223deb8
Morgan Bradford <morganjbradford95@gmail.com>
2022-12-27 15:49:20 -0600

    create UpdateRoom serializer, view, and url

--- backend/serializers.py
+++ backend/serializers.py
@@ -6,10 +6,19 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause',
-                  'votes_to_skip', 'created_at')
+                    'votes_to_skip', 'created_at')
 
 
 class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('guest_can_pause', 'votes_to_skip')
+
+class UpdateRoomSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(validators=[])
+
+    class Meta:
+        model = Room
+        fields = ('code', 'guest_can_pause', 'votes_to_skip')
+
+
