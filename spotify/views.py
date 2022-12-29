e7c20a5d96eb832c20a1e115480737a7f06aa373
Morgan Bradford <morganjbradford95@gmail.com>
2022-12-29 12:23:54 -0600

    call update_or_create_user_token

--- spotify/views.py
+++ spotify/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from decouple import config
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from requests import Request, post
+from .util import update_or_create_user_token
 
 class AuthURL(APIView):
 
@@ -43,4 +44,12 @@ def spotify_callback(request, format=None):
   refresh_token = response.get('refresh_token')
   expires_in = response.get('expires_in')
   error = response.get('error')
+
+  if not request.session.exists(request.session.session_key):
+    request.session.create()
+
+
+  update_or_create_user_token(request.session.session_key, access_token, token_type, expires_in, refresh_token)
+
+  return redirect('frontend:')
 
