d5cc4dd49ee588104d23468a392012f69994160e
Morgan Bradford <morganjbradford95@gmail.com>
2022-12-30 16:53:49 -0600

    correct votes variable

--- spotify/views.py
+++ spotify/views.py
@@ -91,7 +91,7 @@ class CurrentSong(APIView):
         name = artist.get('name')
         artist_string += name
 
-    votes = Vote.objects.filter(room=room, song_id=song_id)
+    votes = len(Vote.objects.filter(room=room, song_id=song_id))
 
     song = {
         'title': item.get('name'),
